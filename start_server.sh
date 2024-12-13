90172aafd1e72138cc48a2c0ec7bc413c4a3057c
kyuyounglee <pia222.20240820@gmail.com>
2024-12-13 14:49:32 +0900

    setting 변경

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ~/venv/board/bin/activate
-python manage.py runserver --settings=config.settings_host 0:8000
+# python manage.py runserver --settings=config.settings_host 0:8000
+python manage.py runserver 0:8000
