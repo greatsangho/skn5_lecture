e9f64e6cf46dee4984271cd38f636e47b299da07
kyuyounglee <pia222.20240820@gmail.com>
2024-12-13 16:13:00 +0900

    aa

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+# settings.py 파일에 추가
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
